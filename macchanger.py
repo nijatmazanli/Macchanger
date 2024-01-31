5646ba9f76e23f6dc2e23e8de2e6bc4597e9b671
Poseydonian.exe <156288746+poseydonianexe@users.noreply.github.com>
2024-01-31 23:02:28 +0400

    Some Bug Fixes and simply colored UI

--- macchanger.py
+++ macchanger.py
@@ -1,61 +1,89 @@
 #!/usr/bin/env python
 
-import subprocess
 import optparse
 import re
+import subprocess
+
+from colorama import Fore, Style
+try:
+    try:
+        def get_options():
+            parser = optparse.OptionParser()
+            parser.add_option("-i", "--interface", dest="interface", help=" Enter the interface")
+            parser.add_option("-m", "--new-mac", dest="mac", help=" Enter the New Mac address")
+            parser.add_option("--manual-mode", dest="manual_mode", help="Enables manuall mode")
+            (options, arguments) = parser.parse_args()
+
+            if not options.interface and options.manual_mode == 1:
+                print(Fore.LIGHTYELLOW_EX + " [*] " + Fore.LIGHTWHITE_EX + " Please enter the interface" + Style.RESET_ALL)
+            elif not options.mac and options.manual_mode == 1:
+                print(Fore.LIGHTYELLOW_EX + " [*] " + Fore.LIGHTWHITE_EX + "Please neter the MAC address" + Style.RESET_ALL)
+
+            return options
 
 
-def get_options():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help=" Enter the interface")
-    parser.add_option("-m", "--new-mac", dest="mac", help=" Enter the New Mac address")
-    parser.add_option("--manual-mode", dest="manual_mode", help="Enables manuall mode")
-    (options, arguments) = parser.parse_args()
-    if not options.interface and not options.manual_mode != 1:
-        print(" [*] Please enter the interface")
-    elif not options.mac and not options.manual_mode != 1:
-        print("[*] Please neter the MAC address")
-
-    return options
+        def change_mac(options):
+            interface = options.interface
+            mac = options.mac
+            try:
+                ifconfig_output2 = subprocess.check_output(["sudo", "ifconfig", interface])
 
 
-def change_mac(options):
-    interface = options.interface
-    mac = options.mac
+            except subprocess.CalledProcessError:
+                print(Fore.RED + " [-] Failed to change MAS address or you entered same MAC address")
+                print(" [-] MAC address not changed" + Style.RESET_ALL)
+                print(
+                    Fore.LIGHTYELLOW_EX + " [*] " + Fore.LIGHTWHITE_EX + " Please enter the real interface"
+                    + Style.RESET_ALL)
+                quit()
 
-    subprocess.call(["sudo", "ifconfig", interface, "down"])
-    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac])
-    subprocess.call(["sudo", "ifconfig", interface, "up"])
+            subprocess.call(["sudo", "ifconfig", interface, "down"])
+            subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac])
+            subprocess.call(["sudo", "ifconfig", interface, "up"])
 
-
-def get_mac(interface):
-    ifconfig_result = str(subprocess.check_output(["sudo", "ifconfig", get_options().interface]))
-    old_mac = re.search(r"..:..:..:..:..:..", ifconfig_result)
-    if old_mac:
-        return old_mac.group(0)
-
-    else:
-        print(" [*] Please enter network interface that actually have MAC address")
+            print(Fore.LIGHTYELLOW_EX + " [+] " + Fore.LIGHTWHITE_EX + "New MAC :: " + Fore.GREEN + mac + Style.RESET_ALL)
+            print(Fore.LIGHTYELLOW_EX + " [*] " + Fore.LIGHTWHITE_EX + "MAC address changed")
 
 
-if get_options().manual_mode == "1":
-    subprocess.call(["sudo", "ifconfig", "-s"])
-    interface = input("Enter the interface :: ")
-    mac = input("Enter new MAC address :: ")
+        def get_mac(interface):
+            ifconfig_result = str(subprocess.check_output(["sudo", "ifconfig", get_options().interface]))
+            old_mac = re.search(r"..:..:..:..:..:..", ifconfig_result)
+            if old_mac:
+                return old_mac.group(0)
 
-    old_mac = ""
-
-    subprocess.call(["sudo", "ifconfig", interface, "down"])
-    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac])
-    subprocess.call(["sudo", "ifconfig", interface, "up"])
-    if old_mac != mac:
-        print(" [+] New MAC :: ", mac)
-        print(" [+] MAC address changed")
-
-    else:
-        print(" [-] Failed to change MAS address or you entered same MAC address")
-        print(" [+] MAC address not changed")
+            else:
+                print(
+                    Fore.LIGHTYELLOW_EX + " [*] " + Fore.LIGHTWHITE_EX + "Please enter network interface that actually have MAC address")
 
 
-else:
-    print(" [+] Please re-run script with -i and -m prefences, like this: ./macchanger.py -i <inteface> -m <mac address>")
+        def manual_mode():
+            print("dsds")
+            subprocess.call(["sudo", "ifconfig", "-s"])
+            interface = input(Fore.CYAN + Style.BRIGHT + "Enter the interface :: " + Style.RESET_ALL)
+            mac = input(Fore.CYAN + "Enter new MAC address :: " + Style.RESET_ALL)
+
+            old_mac = ""
+
+            subprocess.call(["sudo", "ifconfig", interface, "down"])
+            subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac])
+            subprocess.call(["sudo", "ifconfig", interface, "up"])
+            if old_mac != mac:
+                print(
+                    Fore.LIGHTYELLOW_EX + " [+] " + Fore.LIGHTWHITE_EX + "New MAC :: " + Fore.GREEN + mac + Style.RESET_ALL)
+                print(Fore.LIGHTYELLOW_EX + " [*] " + Fore.LIGHTWHITE_EX + "MAC address changed")
+
+            else:
+                print(Fore.RED + " [-] Failed to change MAS address or you entered same MAC address")
+                print(" [+] MAC address not changed" + Style.RESET_ALL)
+
+
+        if get_options().manual_mode == "1":
+            manual_mode()
+        else:
+            change_mac(get_options())
+
+    except TypeError:
+        subprocess.call(["python", "macchanger.py", "--help"])
+except KeyboardInterrupt:
+    print(Fore.YELLOW + "\n  [***] Quitting" + Style.RESET_ALL)
+    quit()
